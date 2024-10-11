fa0c12634b1478814edd8467636ed146757fce3d
kapis <kapis90@gmail.com>
2024-10-11 21:03:08 +0200

    implement multicommand

--- src/iac_jenkins_assistant/__init__.py
+++ src/iac_jenkins_assistant/__init__.py
@@ -1,15 +1,25 @@
 import click
-import requests
 
 
-@click.command()
+@click.group()
+@click.version_option("0.1.0", prog_name="iac_jenkins_assistant")
+def main():
+    click.echo("Running toplevel command")
+
+
+@click.option("--name", required=True, type=str)
+@main.command()
+def create_template(name: str):
+    data = {"name": name}
+    click.echo(data)
+
+
+@main.command()
 @click.option("--template_name", required=True, type=str)
 @click.option("--number_of_machines", required=True, type=int)
-@click.version_option("0.1.0", prog_name="iac_jenkins_assistant")
-def main(template_name: str, number_of_machines: int):
+def deploy_infra(template_name: str, number_of_machines: int):
     data = {"template_name": template_name, "number_of_machines": number_of_machines}
-    response = requests.post("https://httpbin.org/anything", data)
-    click.echo(response.request.body)
+    click.echo(data)
 
 
 if __name__ == "__main__":
