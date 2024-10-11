c6be487896798bf04434c8a954849a77f453c7b7
kapis <kapis90@gmail.com>
2024-10-11 21:12:55 +0200

    modify test file for debugging

--- test/test_iac_jenkins_assistant.py
+++ test/test_iac_jenkins_assistant.py
@@ -2,5 +2,20 @@ from click.testing import CliRunner
 
 from iac_jenkins_assistant import main
 
-runner = CliRunner()
-result = runner.invoke(main, ["--template_name", "test"])
+
+def test_create_template():
+    runner = CliRunner()
+    result = runner.invoke(main, ["create-template", "--name", "test"])
+    pass
+
+
+def test_deploy_infra():
+    runner = CliRunner()
+    result = runner.invoke(
+        main, ["deploy-infra", "--template_name", "test", "--number_of_machines", "4"]
+    )
+    pass
+
+
+test_create_template()
+test_deploy_infra()
